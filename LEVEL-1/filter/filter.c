ae69d14c451154613ac275a668604d603ab76ba7
yusa-a <yusa-a@weseek.co.jp>
2026-01-31 10:16:14 +0900

    v

--- LEVEL-1/filter/filter.c
+++ LEVEL-1/filter/filter.c
@@ -35,77 +35,6 @@ char *ft_strjoin(char *s1, char *s2)
 	return (result);
 }
 
-// void ft_check_replace(char **str, char *arg)
-// {
-// 	char	*res = malloc(strlen(*str) + 1);
-// 	if (!res)
-// 	{
-// 		free(*str);
-// 		perror("Error: ");
-// 		exit(1);
-// 	}
-
-// 	size_t	len = strlen(arg);
-// 	char *src = *str;
-// 	char *dst = res;
-
-// 	while (*src)
-// 	{
-// 		size_t i = 0;
-// 		while (arg[i] && src[i] == arg[i])
-// 			i++;
-// 		if (i == len)
-// 		{
-// 			for (size_t j = 0; j < len; j++)
-// 				*dst++ = '*';
-// 			src += len;
-// 		}
-// 		else
-// 			*dst++ = *src++;
-// 	}
-// 	*dst = '\0';
-
-// 	free(*str);
-// 	*str = res;
-// }
-
-// int main(int ac, char **av)
-// {
-// 	if(ac != 2 || av[1][0] == '\0')
-// 		return 1;
-
-// 	char *str = calloc(1, 1);
-
-// 	char *dest = malloc((size_t)BUFFER_SIZE + 1);
-// 	if(!dest)
-// 		print_error(&dest, &str, 0, 0);
-
-// 	ssize_t line_b = 1;
-// 	while(line_b > 0)
-// 	{
-// 		line_b = read(0, dest, BUFFER_SIZE);
-// 		if (line_b == -1)
-// 			print_error(&dest, &str, 1, 1);
-// 		dest[line_b] = '\0';
-
-// 		char *tmp = ft_strjoin(str, dest); // ?
-// 		if (!tmp)
-// 			print_error(&dest, &str, 1, 1);
-
-// 		free(str);
-// 		str = tmp;
-// 	}
-// 	free(dest);
-
-// 	ft_check_replace(&str, av[1]);
-
-// 	printf("%s", str);
-// 	free(str);
-// 	return (0);
-// }
-
-/////
-
 int main(int ac, char **av)
 {
 	// read all text
@@ -124,6 +53,7 @@ int main(int ac, char **av)
 		line_b = read(0, dest, BUFFER_SIZE);
 		if (line_b == -1)
 			print_error(&dest, &str, 1, 1);
+
 		dest[line_b] = '\0';
 
 		char *tmp = ft_strjoin(str, dest);
@@ -173,7 +103,7 @@ int main(int ac, char **av)
 
 	free(str);
 	str = res;
-
+//
 
 
 	printf("%s", str);
